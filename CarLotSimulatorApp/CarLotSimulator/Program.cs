7ea7c910fb41ea1bc1720f24befd4c7499b857f9
Damion Russell <domino.russell@gmail.com>
2022-07-20 12:10:17 -0400

    initial commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,7 +17,28 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car hondaAccord09 = new Car(2009, "Honda", "Accord", "vroom", "honk", true);
+            Console.WriteLine($"{hondaAccord09.Year} {hondaAccord09.Make} {hondaAccord09.Model} {hondaAccord09.EngineNoise} {hondaAccord09.HonkNoise} {hondaAccord09.IsDriveable}");
 
+            Car buickRegal98 = new Car()
+            {
+                Year = 1998,
+                Make = "Buick",
+                Model = "Regal",
+                EngineNoise = "Purt",
+                HonkNoise = "--",
+                IsDriveable = false
+            };
+            Console.WriteLine($"{buickRegal98.Year} {buickRegal98.Make} {buickRegal98.Model} {buickRegal98.EngineNoise} {buickRegal98.HonkNoise} {buickRegal98.IsDriveable}");
+
+            Car FordFocus12 = new Car();
+            FordFocus12.Year = 2012;
+            FordFocus12.Make = "Ford";
+            FordFocus12.Model = "Focus";
+            FordFocus12.EngineNoise = "Meeeeww";
+            FordFocus12.HonkNoise="meep";
+            FordFocus12.IsDriveable = true;
+            Console.WriteLine($"{FordFocus12.Year} {FordFocus12.Make} {FordFocus12.Model} {FordFocus12.EngineNoise} {FordFocus12.HonkNoise} {FordFocus12.IsDriveable}");
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
